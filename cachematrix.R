aac0164fa29c6a94084da361201c041d37454de7
theam14 <th.emanuel@gmail.com>
2015-05-24 20:14:48 -0300

    Including assignments files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Script used to encapsulate a matrix with contains its own inverse. 
+## The purpose is keep the inverse stored with its already being 
+## calculated and the matrix is not changed, avoiding unecessary
+## calculation.
 
-## Write a short comment describing this function
-
+## Function that creates a list which encapsulates a matrix 
+## into a list structure, that presents the method elements:
+## set - assign a informed matrix
+## get - returns the matrix
+## setInverse - set the inverse of the matrix
+## getInverse - get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(mat) {
+    x <<- mat
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(pinv) inv <<- pinv
+  getInverse <- function() inv
+  list(set = set, 
+       get = get, 
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Perform the inverse calculation of a "encapsulated" 
+## matrix x, which is generated through the function 
+## makeCacheMatrix, and stores the calculated inverse into x.
+## If the inverse was already calculated, the calculation is 
+## not performed.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (is.null(inv))
+  {
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setInverse(inv)  
+  }
+  inv
 }
